e3317271ea25f90abcb07fe5657c189e55d98f36
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-01-04 14:32:45 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -214,6 +214,9 @@ def start(update: Update, context: CallbackContext):
                          InlineKeyboardButton(
                              text="🗄 Source code",
                              url="https://github.com/madboy482/Romeo")
+                         InlineKeyboardButton(
+                             text="Deploy to Heroku",
+                             url="https://heroku.com/deploy?template=https://github.com/madboy482/Romeo-2.0.git")
                      ]]))
     else:
         update.effective_message.reply_text(
@@ -562,3 +565,4 @@ if __name__ == '__main__':
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
     main()
+
